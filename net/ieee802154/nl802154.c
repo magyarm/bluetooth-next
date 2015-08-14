18d5968eb779b5e8e877acb4a13ea64f6788016b
Thomas Yun <thomas.yun@mmbresearch.com>
2015-08-14 12:05:56 -0400

    This sets the short address according to iwpan dev wpan0 info

--- net/ieee802154/nl802154.c
+++ net/ieee802154/nl802154.c
@@ -1324,12 +1324,11 @@ static void nl802154_assoc_cnf( struct genl_info *info, u16 assoc_short_address,
 
 	r = 0;
 	rdev = info->user_ptr[0];
-	wpan_dev = (struct wpan_dev *) &rdev->wpan_phy.dev;
-	dev = (struct net_device *) wpan_dev->netdev;
-
+	dev = info->user_ptr[1];
+	wpan_dev = dev->ieee802154_ptr;
 	r = rdev_set_short_addr( rdev, wpan_dev, assoc_short_address );
 	if ( 0 != r ) {
-		dev_err( &dev->dev, "nla_put_failure (%d)\n", r );
+		dev_err( &dev->dev, "set short addr failure (%d)\n", r );
 		goto out;
     }
     reply = nlmsg_new( NLMSG_DEFAULT_SIZE, GFP_KERNEL );
